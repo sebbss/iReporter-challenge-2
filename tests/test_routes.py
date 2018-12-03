0d3426d072e12d71838a91ac1a5bf682d78b6424
joseph <jsenabulya2@gmail.com>
2018-12-03 21:55:57 +0300

    refactoring code

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,6 +25,12 @@ class TestRoutes(TestCase):
 				'video':'video',
 				'status':'none'
 		}
+		self.flag_data3 ={
+				'description':'misuse of funds'
+		}
+		self.flag_data4 ={
+				'location':'gayaza'
+		}
 	
 
 	def test_FlagCreation(self):
@@ -54,3 +60,23 @@ class TestRoutes(TestCase):
 			resp = cli.delete("/ireporter/api/v1/flags/2")
 			self.assertEqual(resp.status_code, 202)
 
+	def test_update_description(self):
+		with self.test_app as cli:
+			updt = cli.patch("/ireporter/api/v1/flags/1/description", content_type="application/json",
+							 data=json.dumps(self.flag_data3))
+			response = cli.get("/ireporter/api/v1/flags/1")
+			self.assertIn('misuse of funds', str(response.data))
+
+	def test_update_location(self):
+		with self.test_app as cli:
+			updt = cli.patch("/ireporter/api/v1/flags/1/location", content_type="application/json",
+							 data=json.dumps(self.flag_data4))
+			response = cli.get("/ireporter/api/v1/flags/1")
+			self.assertIn('gayaza', str(response.data))
+
+	def test_update_withInvalid_id(self):
+		with self.test_app as cli:
+			updt = cli.patch("/ireporter/api/v1/flags/4/description", content_type="application/json",
+							 data=json.dumps(self.flag_data3))
+			print(updt.data)
+			self.assertIn('the red-flag either doesnot exist or cannot be edited', str(updt.data))
