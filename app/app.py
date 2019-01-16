01b905a0618edb95b075268300ba038452b13100
sebbss <jsenabulya2@gmail.com>
2019-01-16 12:05:32 +0300

    added homepage heroku

--- app/app.py
+++ app/app.py
@@ -1,6 +1,20 @@
+from app.routes.flags import *
 from flask import Flask
 
 
 app = Flask(__name__)
-from app.routes.flags import *
 
+ireporter = '''<!DOCTYPE html>
+				<html lang="en">
+					<head>
+						<meta charset="UTF-8">
+						<title>iReporter</title>
+					</head>
+					<body>
+						<h1>iReporter</h1>
+						<a href="https://joseph-api.herokuapp.com/ireporter/api/v1/flags"></a>
+					</body>
+				</html>'''
+app.route('/')
+def home():
+    return ireporter
