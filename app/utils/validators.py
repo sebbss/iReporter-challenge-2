407fbac7f5bbc1028aed975e92112d3f2aa07ec3
sebbss <jsenabulya2@gmail.com>
2019-01-23 16:15:25 +0300

    [Finishes #163355386]add signup

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -1,22 +1,27 @@
 from flask import jsonify
-def validate_flag(**flag_data):
-	#check empty feilds
-	for value in flag_data.values():
-		if value == "":
-			return jsonify({'error message':'createdBy, location, description,video and image is required'})
+def validate_flag_data(description, video, image, location):
+    if not description:
+        return jsonify({'message':'description cannot be empty'})
+    if not location:
+        return jsonify({'message':'location cannot be empty'})
+    if not isinstance(video,str) and not isinstance(image,str):
+        return jsonify({'message':'video and image have to be strings'})
+	
 
+def validate_user_strings(firstname, lastname, username, phonenumber):
+    if not firstname and not lastname and not username:
+        return jsonify({'message':'firstname, lastname, username cannot be empty'})
+    if not firstname.isalpha():
+        return jsonify({'message':'firstname has to be in alphabetical letters'})
+    if not lastname.isalpha():
+        return jsonify({'message':'lastname has to be in alphabetical letters'})
+    if not username.isalpha():
+        return jsonify({'message':'username has to be in alphabetical letters'})
 
-
-	return None 
-
-def string_validator(string_param):
-    special_characters ='$#@%&*!'
-    special_character = 0
-    for character in string_param:
-       
-        if special_characters.find(character) != -1:
-            special_character +=1
-
-    if special_character >= 1:
-        return "special character exists"
     
+    if len(firstname)>15 or len(lastname)>15 or len(username)>15:
+
+        return jsonify({'message':'firstname or lastname or username shouldnt be more than 15 characters'})
+    if len(phonenumber)>10 or  not phonenumber.isdigit():
+        return jsonify({'message':'phone number has to be in digits and shouldnt exceed 10 digits'})
+
