cae2330e7a0ae9479d59311eea6d97a05b81bb02
sebbss <jsenabulya2@gmail.com>
2019-01-23 18:17:52 +0300

    [Finishes #163355646] add feature get specific intervention

--- app/routes/interventions.py
+++ app/routes/interventions.py
@@ -1,10 +1,10 @@
 from flask import request, jsonify
-from app.models.flags import Flag
+from app.models.interventions import Intervention
 from app.app import app
 from app.utils.validators import validate_flag_data
 from app.utils.helpers import token_required
 from flask_jwt_extended import jwt_required
-red_flag = Flag()
+red_flag = Intervention()
 
 """create an intervention"""
 
@@ -22,12 +22,33 @@ def createFlag(current_user):
 	res = validate_flag_data(description,video,image,location)
 	if res:
 		return res
-	new_flag = red_flag.create_redflag(location, description, video, image,createdby)
+	new_flag = red_flag.create_intervention(location, description, video, image,createdby)
 	return jsonify({
         'status': 201,
         'data': [
             {
                 'id':new_flag[0],
-                'message':'created red-flag'
+                'message':'created intervention'
             }]
     }), 201
+
+
+"""get all interventions"""
+
+
+@app.route("/ireporter/api/v2/interventions")
+@token_required
+def get_interventons(current_user):
+	response = red_flag.get_flags()
+	return jsonify({'status':200 ,'interventions':response}), 200
+
+"""get a specific red-flag"""
+
+
+@app.route("/ireporter/api/v2/intervention/<int:flag_id>")
+@token_required
+def get_anIntervention(flag_id,current_user):
+	flag = intervention.get_anIntervention(flag_id)
+	if flag:
+		return jsonify(flag)
+	return jsonify({'message':'intervention with that id doesnot exist'})
