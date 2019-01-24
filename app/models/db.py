dddbf4c43a99615a99b9e59cf989a9f8299f38cf
sebbss <jsenabulya2@gmail.com>
2019-01-24 19:37:06 +0300

    add heroku link

--- app/models/db.py
+++ app/models/db.py
@@ -9,13 +9,13 @@ class Database():
       if os.getenv('DB_NAME') == 'sebbss':
         self.db = 'sebbss'
         self.connection = psycopg2.connect(dbname=self.db, user= 'postgres', host='localhost',port= '5432')
-      elif:
-        self.db = 'testdb'
-        self.connection = psycopg2.connect(dbname=self.db, user= 'postgres', host='localhost',port= '5432')
-      else:
+      elif os.getenv(DB_NAME) == 'd27hcrnfoaph58':
         self.connection = psycopg2.connect(dbname='d27hcrnfoaph58', user = 'spzzqfyvfsrcyp',
                              host = 'ec2-54-227-246-152.compute-1.amazonaws.com',
                              password='8be1b4979cb14615fdf020b1b15de2410836f50388b9c514ea367fa6f11f2093', url='postgres://spzzqfyvfsrcyp:8be1b4979cb14615fdf020b1b15de2410836f50388b9c514ea367fa6f11f2093@ec2-54-227-246-152.compute-1.amazonaws.com:5432/d27hcrnfoaph58',port = '5432' )
+      else:
+        self.db = 'testdb'
+        self.connection = psycopg2.connect(dbname=self.db, user= 'postgres', host='localhost',port= '5432')
       
       self.connection.autocommit = True
       self.cursor = self.connection.cursor()
